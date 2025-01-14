171680dead199ccc9e6dd1134a0d32bc143353c9
DevHassan101 <hassanshaikh311203@gmail.com>
2025-01-14 21:22:37 +0500

    changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <script src="https://cdn.tailwindcss.com"></script>
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
